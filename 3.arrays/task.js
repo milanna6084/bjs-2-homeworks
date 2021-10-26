4f24f6e795b16fd02643c9c63a4ff9822f992684
milanna6084 <molodojenyuk.ann@gmail.com>
2021-10-26 10:45:56 +0500

    Tasks 1-2 Arrays

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,17 @@
 function compareArrays(arr1, arr2) {
   let result;
 
-  // Ваш код
+  if (arr1.length === arr2.length) {
+    result = arr1.every((item, index) => item === arr2[index]);
+  } else result = false;
 
   return result; // boolean
 }
 
 function advancedFilter(arr) {
-  let resultArr;
-
-  // Ваш код
+  let resultArr = arr.filter((item) => item > 0)
+  .filter((item) => item % 3 === 0)
+  .map((item) => item * 10);
 
   return resultArr; // array
 }
