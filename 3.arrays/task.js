6506a519aa82b1d2d773c1eb23cbfc8e0a7b5ce6
milanna6084 <molodojenyuk.ann@gmail.com>
2021-10-26 12:21:42 +0500

    Corrected tasks 1-2 Arrays

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,17 +1,15 @@
 function compareArrays(arr1, arr2) {
-  let result;
-
-  if (arr1.length === arr2.length) {
-    result = arr1.every((item, index) => item === arr2[index]);
-  } else result = false;
-
-  return result; // boolean
+  return arr1.length === arr2.length
+    
+         && arr1.every((item, index) => item === arr2[index]);
+  
+   // boolean
 }
 
 function advancedFilter(arr) {
-  let resultArr = arr.filter((item) => item > 0)
+  return arr.filter((item) => item > 0)
   .filter((item) => item % 3 === 0)
   .map((item) => item * 10);
 
-  return resultArr; // array
+   // array
 }
