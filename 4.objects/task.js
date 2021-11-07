502ade29fbf42b620abb7fefb96c4ddd2e900482
milanna6084 <molodojenyuk.ann@gmail.com>
2021-11-07 18:03:45 +0500

    Task 1 Objects

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,39 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+  this.subject = subjectName;
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function (mark) {
+  if (this.marks === undefined) { 
+    this.marks = [mark];
+  }
+  else {
+      this.marks.push(mark);
+  }
+
+}
+
+Student.prototype.addMarks = function (...marks) {  
+  if (this.marks === undefined) { 
+    this.marks = marks;
+  } 
+  else {
+      this.marks.contact(marks);
+  }
+
+}
+
+Student.prototype.getAverage = function () {
+   return this.marks.reduce((sum, mark) => sum + mark) / this.marks.length;
+}
+
+Student.prototype.exclude = function (reason) {
+  delete this.marks;
+  delete this.subject;
+  this.excluded = reason;
+}
