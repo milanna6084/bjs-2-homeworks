118221c939c48b9179ffffc8127ff46226324c0a
milanna6084 <molodojenyuk.ann@gmail.com>
2021-11-22 17:35:30 +0500

    Task 1-3 Decorators

--- 8.decorators/main.js
+++ 8.decorators/main.js
@@ -1,20 +1,28 @@
 const addThree = (a, b, c) => (a + b + c); 
 const upgradedAddThree = cachingDecoratorNew(addThree);
-upgradedAddThree(1, 2, 3); 
-upgradedAddThree(1, 2, 3); 
+upgradedAddThree(1, 2, 3);
+upgradedAddThree(1, 2, 3);
 upgradedAddThree(2, 2, 3); 
 upgradedAddThree(3, 2, 3); 
 upgradedAddThree(4, 2, 3); 
 upgradedAddThree(5, 2, 3); 
 upgradedAddThree(6, 2, 3); 
-upgradedAddThree(1, 2, 3); 
+upgradedAddThree(1, 2, 3);
 
 const sendSignal = () => console.log('Сигнал послан'); 
-const upgradedSendSignal = debounceDecoratorNew(sendSignal,2000);
-setTimeout(upgradedSendSignal()); 
-setTimeout(upgradedSendSignal(),300); 
-setTimeout(upgradedSendSignal(),900); 
-setTimeout(upgradedSendSignal(),1200);
-setTimeout(upgradedSendSignal(),2300);
-setTimeout(upgradedSendSignal(),4400);
-setTimeout(upgradedSendSignal(),4500);
+const upgradedSendSignal = debounceDecorator2(sendSignal,2000);
+
+setTimeout(upgradedSendSignal); 
+setTimeout(upgradedSendSignal,300, "300"); 
+setTimeout(upgradedSendSignal,900, "900"); 
+setTimeout(upgradedSendSignal,1200, "1200");
+setTimeout(upgradedSendSignal,2300, "2300");
+setTimeout(upgradedSendSignal,4400, "4400");
+setTimeout(upgradedSendSignal,4500, "4500");
+setTimeout(() => console.log(`Было сделано ${upgradedSendSignal.count} вызовов`), 7000);
+
+
+
+
+
+
--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,12 +1,78 @@
 function cachingDecoratorNew(func) {
-  // Ваш код
+    let cash = [];
+
+    function wrapper(...args) {
+    
+        if (cash.length >= 5 ) {
+            cash.shift(); 
+        }
+
+        let hash = args.join(",");
+        let index = cash.findIndex((item) => item.hash === hash); 
+
+        if (index !== -1) {
+           console.log("Из кэша: " + cash[index].result);
+           return("Из кэша: " + cash[index].result);
+        }
+
+        else {
+        let result = func(...args); 
+        cash.push({hash,result});
+        console.log("Вычисляем: " + result);
+        return("Вычисляем: " + result);
+        }
+    }
+
+    return wrapper;
+}
+  
+
+function debounceDecoratorNew(func, ms) {
+  let timer;  
+
+  function wrapper(str) {    
+    if (timer) {
+      clearTimeout(timer);  
+      console.log(str);   
+      timer = setTimeout(() => {
+        func();
+      }, ms);
+      return;
+    }
+    else {
+      func(); 
+      
+      timer = setTimeout(() => {
+        func();
+        }, ms); 
+    } 
+  }
+  return wrapper;
 }
 
 
-function debounceDecoratorNew(func) {
-  // Ваш код
+function debounceDecorator2(func, ms) {
+  let timer;  
+
+  function wrapper(str) { 
+    wrapper.count++;   
+    if (timer) {
+      clearTimeout(timer);   
+      timer = setTimeout(() => {
+        func();
+      }, ms);
+      return;
+    }
+    else {
+      func(); 
+      
+      timer = setTimeout(() => {
+        func();
+        }, ms); 
+    } 
+  }
+  wrapper.count = 0;
+  return wrapper;
 }
 
-function debounceDecorator2(func) {
-  // Ваш код
-}
+
