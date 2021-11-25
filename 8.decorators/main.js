c4a94661ef06de84a83b7cc303b27a184746d432
milanna6084 <molodojenyuk.ann@gmail.com>
2021-11-25 08:33:13 +0500

    Corrected tasks 1-3 Decorators

--- 8.decorators/main.js
+++ 8.decorators/main.js
@@ -13,13 +13,15 @@ const sendSignal = () => console.log('Сигнал послан');
 const upgradedSendSignal = debounceDecorator2(sendSignal,2000);
 
 setTimeout(upgradedSendSignal); 
-setTimeout(upgradedSendSignal,300, "300"); 
-setTimeout(upgradedSendSignal,900, "900"); 
-setTimeout(upgradedSendSignal,1200, "1200");
-setTimeout(upgradedSendSignal,2300, "2300");
-setTimeout(upgradedSendSignal,4400, "4400");
-setTimeout(upgradedSendSignal,4500, "4500");
-setTimeout(() => console.log(`Было сделано ${upgradedSendSignal.count} вызовов`), 7000);
+setTimeout(upgradedSendSignal,300); 
+setTimeout(upgradedSendSignal,900); 
+setTimeout(upgradedSendSignal,1200);
+setTimeout(upgradedSendSignal,2300);
+setTimeout(upgradedSendSignal,4400);
+setTimeout(upgradedSendSignal,4500);
+setTimeout(() => console.log(`Было сделано ${upgradedSendSignal.count} вызов(-a)(-oв)`), 7000);
+
+
 
 
 
--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,26 +1,23 @@
 function cachingDecoratorNew(func) {
     let cash = [];
 
-    function wrapper(...args) {
-    
-        if (cash.length >= 5 ) {
-            cash.shift(); 
-        }
+    function wrapper(...args) {    
+      if (cash.length >= 5 ) {
+          cash.shift(); 
+      }
 
-        let hash = args.join(",");
-        let index = cash.findIndex((item) => item.hash === hash); 
+      let hash = args.join(",");
+      let index = cash.findIndex((item) => item.hash === hash); 
 
-        if (index !== -1) {
-           console.log("Из кэша: " + cash[index].result);
-           return("Из кэша: " + cash[index].result);
-        }
+      if (index !== -1) {
+          console.log("Из кэша: " + cash[index].result);
+          return("Из кэша: " + cash[index].result);
+      }
 
-        else {
-        let result = func(...args); 
-        cash.push({hash,result});
-        console.log("Вычисляем: " + result);
-        return("Вычисляем: " + result);
-        }
+      let result = func(...args); 
+      cash.push({hash,result});
+      console.log("Вычисляем: " + result);
+      return("Вычисляем: " + result);        
     }
 
     return wrapper;
@@ -28,24 +25,15 @@ function cachingDecoratorNew(func) {
   
 
 function debounceDecoratorNew(func, ms) {
-  let timer;  
+  let timer; 
 
-  function wrapper(str) {    
-    if (timer) {
-      clearTimeout(timer);  
-      console.log(str);   
-      timer = setTimeout(() => {
-        func();
-      }, ms);
-      return;
-    }
-    else {
-      func(); 
-      
-      timer = setTimeout(() => {
-        func();
-        }, ms); 
-    } 
+  function wrapper(...args) {     
+    if(!timer) func(...args); 
+
+    clearTimeout(timer);  
+    timer = setTimeout(() => {
+      func(...args)
+    }, ms);
   }
   return wrapper;
 }
@@ -54,24 +42,19 @@ function debounceDecoratorNew(func, ms) {
 function debounceDecorator2(func, ms) {
   let timer;  
 
-  function wrapper(str) { 
-    wrapper.count++;   
-    if (timer) {
-      clearTimeout(timer);   
-      timer = setTimeout(() => {
-        func();
-      }, ms);
-      return;
+  function wrapper(...args) {        
+    if(!timer){
+      func(...args); 
+      wrapper.count++;
     }
-    else {
-      func(); 
-      
-      timer = setTimeout(() => {
-        func();
-        }, ms); 
-    } 
-  }
-  wrapper.count = 0;
+
+    clearTimeout(timer);  
+    timer = setTimeout(() => {
+      func(...args);
+      wrapper.count++;
+      }, ms);
+    }
+  wrapper.count = 0;    
   return wrapper;
 }
 
